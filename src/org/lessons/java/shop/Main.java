be31ca5bc2084d2d4581fd4fad7b1e11c1fa5df5
donatomontrone <donato.montrone@outlook.com>
2023-05-08 16:21:50 +0200

    added methods con Prodotto class

---
+++ src/org/lessons/java/shop/Main.java
@@ -0,0 +1,18 @@
+package org.lessons.java.shop;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		
+		Prodotto p1 = new Prodotto("Computer", "Descrizione del primo prodotto", 3d, 10 );
+
+		Prodotto p2 = new Prodotto("Playstation", "Descrizione del secondo prodotto", 3d, 20 );
+
+		
+		System.out.println(p1);
+		System.out.println(p2);
+		
+		
+	}
+	
+}
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,5 +1,87 @@
 package org.lessons.java.shop;
 
-public class Prodotto {
+import java.util.Random;
 
+public class Prodotto {
+	private int code;
+	private String name;
+	private String description;
+	private double price;
+	private int vat;
+	
+	
+	public Prodotto(String name, String description, double price, int vat) {
+		
+		setRandomCode();
+		setName(name);
+		setDescription(description);
+		setPrice(price);
+		setVat(vat);
+	}
+	
+	public int getCode() {
+		return code;
+	}
+	
+	
+	public String getName(){
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getDescription(){
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	
+	public void setPrice(double price){
+		this.price = price;
+	}
+	
+	public int getVat(){
+		return vat;
+	}
+	
+	public void setVat(int vat) {
+		this.vat = vat;
+	}
+	
+	
+	public int setRandomCode() {
+		Random rnd = new Random();
+		return code = rnd.nextInt(20);	
+		
+	}
+	
+	
+	public double vatPrice() {
+		return price + (price * vat / 100);
+	}
+	
+	public String codeName() {
+		return getLongCode() + getName();
+	}
+	
+	public String getLongCode() {
+		return String.format("%08d", code);
+
+	}
+	
+	@Override
+	public String toString() {
+		return "[" + codeName() + "] - prezzo "
+				+ String.format("%.2f", getPrice()) + "€ - IVA "
+				+ getVat() + "% - prezzo con iva "
+				+ vatPrice() + "€";
+	}
 }
